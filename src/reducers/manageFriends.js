111af4e5ab12fb6fe174587d2351a1589b7f944d
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-09-13 14:09:11 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,43 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] }
+    case "REMOVE_FRIEND":
+      let removeFriendArr = state.friends.filter(
+        friend => {
+          // console.log(friend.id !== action.id)
+          return action.id !== friend.id
+        }
+      )
+      // console.log(removeFriendArr)
+      return { friends: removeFriendArr }
+    default:
+      return state
+  }
 }
+
+// let state = {
+//   friends: [
+//     {
+//       name: "Avi",
+//       hometown: "NYC",
+//       id: 100
+//     },
+//     {
+//       name: "Joe",
+//       hometown: "Boston",
+//       id: 101
+//     },
+//     {
+//       name: "Steven",
+//       hometown: "Philadephia",
+//       id: 102
+//     }
+//   ]
+// }
+
+// console.log(manageFriends(state, {
+//   type: "REMOVE_FRIEND",
+//   id: 101
+// }))
+
