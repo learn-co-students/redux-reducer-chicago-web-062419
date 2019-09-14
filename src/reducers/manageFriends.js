831b41b1abde9824a65e89c6a296981f7d665be5
github username <github email address>
2019-09-13 23:11:28 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+        // spread operator to make a nondesctrive copy of the friends array and add the new object denoted by action.friend
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            // iterates over the friends array and keeps all the objects that do NOT equal the id of the friend that the action was made on.
+            return {friends: state.friends.filter(friend => friend.id !== action.id)}
+        default: 
+            return {friends: state.friends}
+      }
+      
 }
+
+
+// switch (action.type) {
+//     case 'INCREASE':
+//       return {numberOfPresents: state.numberOfPresents + 1}
+//     default:
+//       return state;
+//   }
+
+
+  
